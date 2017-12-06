a059332a3889cea8bd6c3a9e46f491eb17ba5586
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2017-12-07 00:22:06 +0200

    day 6 - performance optimization

--- src/aoc/day6.clj
+++ src/aoc/day6.clj
@@ -6,22 +6,19 @@
 
 (defn transfer [memory-count
                 start-index
-                max-amount
+                max-amount,
+                transfer-amount
                 index amount]
   (let [offset (- index start-index)
         circular-offset
         (if (< offset 0)
           (+ (- memory-count start-index) index)
-          offset)
-        amount-for-bank (max 1
-          (if (= (mod max-amount memory-count) 0)
-              (quot max-amount memory-count)
-              (quot max-amount (dec memory-count))))]
+          offset)]
     (cond
       (= offset 0)
       (- amount (min max-amount
-                     (* amount-for-bank (dec memory-count))))
-      (<= circular-offset max-amount) (+ amount amount-for-bank)
+                     (* transfer-amount (dec memory-count))))
+      (<= circular-offset max-amount) (+ amount transfer-amount)
       :else amount)))
 
 (defn redistribute [memory]
@@ -29,12 +26,19 @@
 
         [max-memory-bank-index max-memory-bank-amount]
         (last (sort-by #(vec (reverse (update % 0 (partial * -1))))
-                 (map-indexed vector memory)))]
+                 (map-indexed vector memory)))
+
+        transfer-amount
+        (max 1
+          (if (= (mod max-memory-bank-amount memory-count) 0)
+            (quot max-memory-bank-amount memory-count)
+            (quot max-memory-bank-amount (dec memory-count))))]
 
     (vec (map-indexed (partial transfer
                                memory-count
                                max-memory-bank-index
-                               max-memory-bank-amount)
+                               max-memory-bank-amount,
+                               transfer-amount)
                       memory))))
 
 (defn redistribution [memory] (iterate redistribute memory))
@@ -44,17 +48,19 @@
 
 (defn loop-detection [memory]
   (let [r (redistribution memory)]
-    (dec (c/find-first #(apply (complement distinct?)
-                               (take % r))
-                (drop 1 (range))))))
+    (reduce (fn [past memory]
+              (if (contains? past memory)
+                (reduced (count past))
+                (conj past memory))) #{} r)))
 
 ;; Result - part 1
 ; (loop-detection (parse-memory input))
 ; => 11137
 
 ;; Result - part 2
-; (nth (redistribution (parse-memory input)) 11138)
-; (c/find-first #(= (second %) [0 14 13 12 10 9 9 7 7 5 5 4 2 2 1 12])
-;   (map-indexed vector (redistribution (parse-memory input))))
+; (let [memory-loop (nth (redistribution (parse-memory input)) 11138)]
+;   (c/find-first
+;     #(= (second %) memory-loop)
+;     (map-indexed vector (redistribution (parse-memory input)))))
 ; (- 11138 10101)
 ; => 1037
